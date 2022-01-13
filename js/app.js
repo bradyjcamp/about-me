eeec2ae70ee41386cb41f47f83a2fb44f487f571
Brady Camp <bradyjcamp@gmail.com>
2022-01-12 21:40:19 -0500

    added more questions to initial prompts

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
+let userScore = 0;
 let user = prompt('What is your name?');
-
+document.getElementById('name').innerHTML = user;
 alert('Welcome ' + user + ', glad you are here! Please answer these next questions using "yes" or "no", or "y" or "n".');
 
 let answerOne = prompt('Am I from Washington?').toLowerCase();
 
 if(answerOne === 'yes' || answerOne === 'y') {
+  userScore++;
   alert('That is correct!');
   // console.log('That is correct!');
 }
@@ -17,9 +19,10 @@ else if(answerOne === 'no' || answerOne === 'n') {
   // console.log('Please follow the instructions and answer yes or no');
   alert('Please follow the instructions and answer yes or no');
 }
-let answerTwo = prompt('Do I have any siblings?');
+let answerTwo = prompt('Do I have any siblings?').toLowerCase();
 
 if(answerTwo === 'yes' || answerTwo === 'y') {
+  userScore++;
   alert('Correct! I have two, an older brother, and an older sister. Yes, I am the youngest therefore the favorite.');
   // console.log('Correct! I have two, an older brother, and an older sister. Yes, I am the youngest therefore the favorite.');
 }
@@ -31,9 +34,10 @@ else if(answerTwo === 'no' || answerTwo === 'n') {
   alert('Please follow the instructions and answer yes or no');
 }
 
-let answerThree = prompt('Am I at least 6 feet tall?');
+let answerThree = prompt('Am I at least 6 feet tall?').toLowerCase();
 
 if(answerThree === 'yes' || answerThree === 'y') {
+  userScore++;
   alert('I wish... I am just shy coming in at 5 feet 10 inches.');
   // console.log('I wish... I am just shy coming in at 5 feet 10 inches.');
 }
@@ -45,9 +49,10 @@ else if(answerThree === 'no' || answerThree === 'n') {
   alert('Please follow the instructions and answer yes or no');
 }
 
-let answerFour = prompt('Do I have a college degree?');
+let answerFour = prompt('Do I have a college degree?').toLowerCase();
 
 if(answerFour === 'yes' || answerFour === 'y') {
+  userScore++;
   alert('Indeed I do! I have my Bachelors Degree in Business Management.');
   // console.log('Indeed I do! I have my Bachelors Degree in Business Management.');
 }
@@ -59,10 +64,10 @@ else if(answerFour === 'no' || answerFour === 'n') {
   alert('Please follow the instructions and answer yes or no');
 }
 
-
-let answerFive = prompt('Am I a good artist?');
+let answerFive = prompt('Am I a good artist?').toLowerCase();
 
 if(answerFive === 'yes' || answerFive === 'y') {
+  userScore++;
   alert('Not even close! If you could take one look at my handwriting you would know that I am no artist.');
   // console.log('Not even close! If you could take one look at my handwriting you would know that I am no artist.');
 }
@@ -73,7 +78,67 @@ else if(answerFive === 'no' || answerFive === 'n') {
   // console.log('Please follow the instructions and answer yes or no');
   alert('Please follow the instructions and answer yes or no');
 }
-alert('Enough with the pop quiz. Come check out my website, ' + user + ', and learn some more about me!');
+alert('This is fun! Let\'s keep it going, ' + user + '!');
 
+let numberGuesses = 4;
+let myNumber = 4;
+let rightNumber = false;
 
-document.getElementById('name').innerHTML = user;
+while (numberGuesses && !rightNumber) {
+  let userGuess = prompt('What number am I thinking of between 1 and 10?');
+  numberGuesses--;
+  console.log(typeof(userGuess));
+  if (+userGuess === myNumber) {
+    userScore++;
+    rightNumber = true;
+    alert('You got it right!');
+  } else if (userGuess < myNumber) {
+    alert('Too low!');
+  } else if (+userGuess > myNumber) {
+    alert('Too high!');
+  } if(numberGuesses === 0){
+    alert('Whoops! Too many guesses!');
+  }
+}
+
+let favFood = ['raising cane\'s', 'chick-fil-a', 'taco bell'];
+let guessLimit = 6;
+
+circle: while(guessLimit){
+  let foodGuess = prompt('What is one of my top three favorite fast food restaurants?').toLowerCase();
+  guessLimit--;
+  for (let i = 0; i < favFood.length; i++){
+    if (foodGuess === favFood[i]){
+      userScore++;
+      alert('That is correct! Acceptable answers were raising cane\'s, chick-fil-a, and taco bell!');
+      break circle;
+    }
+  }
+  alert('Oops, you are wrong.');
+}
+if(guessLimit === 0){
+  alert('Out of guesses!');
+}
+alert('Here is your final score' + user + '. You got ' + userScore + ' out of 7 correct!');
+
+// let rightAnswer = false;
+// while (guessLimit && !rightAnswer) {
+//   let foodGuess = prompt(
+//     'What is one of my top three favorite fast food restaurants?').toLowerCase();
+//   guessLimit--;
+//   for (let i = 0; i < favFood.length; i++) {
+//     if (foodGuess === favFood[i]) {
+//       rightAnswer = true;
+//       alert(
+//         'That is correct! All available options were raising cane\'s, chick-fil-a, and taco bell.'
+//       ); break;
+//     } if (foodGuess !== favFood[i]) {
+//       alert('Not quite!');
+//     }
+//   }
+//   if (guessLimit === 0) {
+//     alert(
+//       'Out of guesses! Available options were raising cane\'s, chick-fil-a, and taco bell.'
+//     );
+//   }
+// }
